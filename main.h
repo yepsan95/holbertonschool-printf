4379a3879a37f227b72690b494313585601ffb89
Bryan Yep <yepsan95@gmail.com>
2023-03-28 13:43:11 -0700

    main.h

--- main.h
+++ main.h
@@ -6,6 +6,10 @@ typedef struct Types {
         char type;
         void (*func)(va_list *);
 } types;
+int _putchar(char c);
 int _printf(const char *format, ...);
+void c_id(char c);
+void s_id(char *s);
+void d_id(int n);
 
 #endif
