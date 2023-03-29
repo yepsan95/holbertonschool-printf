81a3584f5ebcc9b5c8e3ffc47bbad5066ddbf031
Bryan Yep <yepsan95@gmail.com>
2023-03-29 16:13:33 -0700

    Specifiers functions

--- id_functions.c
+++ id_functions.c
@@ -125,3 +125,82 @@ void b_id(va_list *p, unsigned int *count)
 	}
 	free(buffer);
 }
+
+/**
+ * u_id - prints an unsigned int argument in decimal
+ * @p: pointer to variable
+ * @count: counts the number of characters printed
+ *
+ * Return: void
+ */
+void u_id(va_list *p, unsigned int *count)
+{
+	unsigned int n, i, j, size, digit;
+
+	n = va_arg(*(p), int);
+
+	if (n == 0)
+	{
+		_putchar(48);
+		*(count) = *(count) + 1;
+	}
+
+	size = 0;
+	i = n;
+	while (i != 0)
+	{
+		i = i / 10;
+		size++;
+	}
+	for (i = size; i > 0; i--)
+	{
+		digit = n;
+		for (j = i; j > 1; j--)
+		{
+			digit = digit / 10;
+		}
+		digit = digit % 10;
+		_putchar(digit + 48);
+		*(count) = *(count) + 1;
+	}
+}
+
+/**
+ * o_id - prints an unsigned int argument in octal
+ * @p: pointer to variable
+ * @count: counts the number of characters printed
+ *
+ * Return: void
+ */
+void o_id(va_list *p, unsigned int *count)
+{
+	unsigned int b, i;
+	char *buffer;
+	int len;
+
+	b = va_arg(*(p), unsigned int);
+	i = b;
+	len = 0;
+
+	while (i != 0)
+	{
+		i = i / 8;
+		len++;
+	}
+	buffer = (char *)malloc((len + 1) * sizeof(char));
+	if (buffer == NULL)
+		return;
+	buffer[len] = '\0';
+	while (b != 0)
+	{
+		len--;
+		buffer[len] = ((b % 8) + 48);
+		b = b / 8;
+	}
+	for (i = 0; buffer[i] != '\0'; i++)
+	{
+		_putchar(buffer[i]);
+		*(count) = *(count) + 1;
+	}
+	free(buffer);
+}
