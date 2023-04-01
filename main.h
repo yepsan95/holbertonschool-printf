5729fb6c3fb5aabf7dc7f171b642457993d15974
Bryan Yep <yepsan95@gmail.com>
2023-03-31 21:03:53 -0700

    Added %S specifier

--- main.h
+++ main.h
@@ -10,19 +10,20 @@
 typedef struct Types {
 
         char type;
-        void (*func)(va_list *, char *, unsigned int *);
+        void (*func)(va_list *, unsigned char *, unsigned int *);
 } types;
 int _putchar(char c);
 int _printf(const char *format, ...);
-void clean_buffer(char *buffer, unsigned int i);
+void clean_buffer(unsigned char *buffer, unsigned int i);
 
-void c_id(va_list *p, char *buffer, unsigned int *i);
-void s_id(va_list *p, char *buffer, unsigned int *i);
-void d_id(va_list *p, char *buffer, unsigned int *i);
-void b_id(va_list *p, char *buffer, unsigned int *i);
-void u_id(va_list *p, char *buffer, unsigned int *i);
-void o_id(va_list *p, char *buffer, unsigned int *i);
-void x_id(va_list *p, char *buffer, unsigned int *i);
-void X_id(va_list *p, char *buffer, unsigned int *i);
+void c_id(va_list *p, unsigned char *buffer, unsigned int *i);
+void s_id(va_list *p, unsigned char *buffer, unsigned int *i);
+void S_id(va_list *p, unsigned char *buffer, unsigned int *i);
+void d_id(va_list *p, unsigned char *buffer, unsigned int *i);
+void b_id(va_list *p, unsigned char *buffer, unsigned int *i);
+void u_id(va_list *p, unsigned char *buffer, unsigned int *i);
+void o_id(va_list *p, unsigned char *buffer, unsigned int *i);
+void x_id(va_list *p, unsigned char *buffer, unsigned int *i);
+void X_id(va_list *p, unsigned char *buffer, unsigned int *i);
 
 #endif
