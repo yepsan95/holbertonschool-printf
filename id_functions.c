012d14259c966cf05d11af1a85c87cb7c2a6f5bd
Bryan Yep <yepsan95@gmail.com>
2023-04-01 19:50:13 -0700

    Added %p specifier

--- id_functions.c
+++ id_functions.c
@@ -353,3 +353,54 @@ void X_id(va_list *p, unsigned char *buffer, unsigned int *i)
 	}
 	free(buf);
 }
+
+/**
+ * p_id - stores a pointer variable converted to hexadecimal in a buffer
+ *        (abcdef in lower case)
+ * @p: pointer to variable
+ * @buffer: buffer where the characters are stored
+ * @i: current index of the buffer to where the characters should be written
+ *
+ * Return: void
+ */
+void p_id(va_list *p, unsigned char *buffer, unsigned int *i)
+{
+	unsigned int x, k;
+	char *buf;
+	int len;
+
+	x = va_arg(*(p), unsigned int);
+	k = x;
+	len = 0;
+
+	if (k == 0)
+	{
+		buffer[(*i)++] = '0';
+		return;
+	}
+	while (k != 0)
+	{
+		k = k / 16;
+		len++;
+	}
+	buf = (char *)malloc((len + 1) * sizeof(char));
+	if (buf == NULL)
+		return;
+	buf[len] = '\0';
+	buffer[(*i)++] = '0';
+	buffer[(*i)++] = 'x';
+	while (x != 0)
+	{
+		len--;
+		buf[len] = ((x % 16) + 48);
+		x = x / 16;
+	}
+	for (k = 0; buf[k] != '\0'; k++)
+	{
+		if (buf[k] <= '9')
+			buffer[(*i)++] = buf[k];
+		else if (buf[k] > '9')
+			buffer[(*i)++] = buf[k] + 39;
+	}
+	free(buf);
+}
